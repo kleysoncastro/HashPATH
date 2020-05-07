2aab9c3db26b09670108289b804f8ce6dab42552
kleson castro <kleysoncastro99@gmail.com>
2020-05-07 14:36:07 -0400

    add function for gererate hash file

--- src/br/com/castro/application/Main.java
+++ src/br/com/castro/application/Main.java
@@ -1,17 +1,27 @@
 package br.com.castro.application;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import br.com.castro.hash.Hash;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Hash hash = new Hash();
 		
-	byte[] hashByte =	hash.generateHash("password", "SHA-256");
+		File f = new File("/home/castro/Downloads/jdk-11.0.7_linux-x64_bin.tar.gz");
+		
+	byte[] hashByte =	hash.generateHash(f, "SHA-256");
+	byte[] hashByte2 =	hash.generateHash("password", "SHA-256");
 	
 	System.out.println(hash.hashToHex(hashByte));
-	//hash.hashToHex(hashByte);
+	System.out.println(hash.hashToHex(hashByte2));
+	
+	
 	
 
 	}
--- src/br/com/castro/hash/Hash.java
+++ src/br/com/castro/hash/Hash.java
@@ -1,5 +1,10 @@
 package br.com.castro.hash;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -9,6 +14,23 @@ public class Hash {
 		  try {
 		    MessageDigest md = MessageDigest.getInstance(algoritmo);
 		    md.update(data.getBytes());
+		    return md.digest();
+		  } catch (NoSuchAlgorithmException e) {
+		    return null;
+		  }
+		}
+	
+	public static byte[] generateHash(File file, String algoritmo) throws IOException {
+		int read = 0;
+		InputStream is = new FileInputStream(file);
+		byte[] buffer = new byte[8192];
+		  try {
+		    MessageDigest md = MessageDigest.getInstance(algoritmo);
+		    
+		    
+		    while( (read = is.read(buffer)) > 0) {
+		    	md.update(buffer, 0, read);
+			}	
 		    return md.digest();
 		  } catch (NoSuchAlgorithmException e) {
 		    return null;
